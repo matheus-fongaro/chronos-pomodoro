8639cdc98a056d26b95fd1a5d07d8c1f1ee1c109
Matheus Henrique Otenio Fongaro <matheus.fongaro@mova.vc>
2025-08-27 12:21:51 -0400

    feat: change app to show components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Container } from './components/common/Container/';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { Countdown } from './components/Countdown';
+import { TaskForm } from './components/TaskForm';
+import { CustomFooter } from './components/CustomFooter';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -13,6 +16,15 @@ export function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <Countdown />
+      </Container>
+      <Container>
+        <TaskForm />
+      </Container>
+      <Container>
+        <CustomFooter />
       </Container>
     </>
   );
